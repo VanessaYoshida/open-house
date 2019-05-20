1ef69665c50e0b61ea9620f43532ab8c027cb20f
Vanessa <vanessayoshida92@gmail.com>
2019-05-20 17:02:41 -0300

    Alteraçao para pegar localizaçao atual, e estou tentando juntar com a funçao das minhas quadras

--- public/src/scripts/maps/findCourts.js
+++ public/src/scripts/maps/findCourts.js
@@ -1,64 +1,28 @@
 function searchPlaces(platform) {
-    // creates a Search entypoint
     var search = new H.places.Search(platform.getPlacesService());
-    // creates parameters for search request
     var params = {
-        'q': 'quadras',
-        'at': '-23.5505,-46.6333'
+        'q': 'quadra',
+        'at': '-23,55,-46,63'
     };
-    // creates a request with callbacks
     search.request(params, {}, onResult, onError);
 }
 
-
-/**
-* This function will be called once the Places REST API provides a response
-* @param  {Object} result          A JSONP object representing the  location(s) found.
-*
-* see: http://developer.here.com/rest-apis/documentation/places/topics_api/media-type-search.html
-*
-*/
 function onResult(result) {
     var places = result.results.items;
-    /*
-    * The styling of the places response on the map is entirely under the developer's control.
-    * A representative styling can be found the full JS + HTML code of this example
-    * in the functions below:
-    */
     addPlacesToMap(places);
     addPlacesToPanel(places);
 }
 
-
-/**
-* This function will be called if a communication error occurs during the JSON-P request
-* @param  {Object} error  The error message received.
-*
-* see: see: http://developer.here.com/rest-apis/documentation/places/topics_api/object-error.html
-*/
 function onError(error) {
     error = data;
 }
 
-
-/**
-* Boilerplate map initialization code starts below:
-*/
-
-
-// Hold a reference to any infobubble opened
 var bubble;
 
-/**
-* Opens/Closes a infobubble
-* @param  {H.geo.Point} position     The location on the map.
-* @param  {String} text              The contents of the infobubble.
-*/
 function openBubble(position, text){
     if(!bubble){
         bubble =  new H.ui.InfoBubble(
             position,
-            // The FO property holds the province name.
             {content: text});
             ui.addBubble(bubble);
         } else {
@@ -68,15 +32,8 @@ function openBubble(position, text){
         }
     }
     
-    
-    
-    /**
-    * Creates a series of clickable markers for each place found  and adds it to the map.
-    * @param {Object[]} places An array of places as received from the H.service.getPlacesService
-    */
     function addPlacesToMap(places) {
         var group = new  H.map.Group();
-        // add 'tap' event listener, that opens info bubble, to the group
         group.addEventListener('tap', function (evt) {
             map.setCenter(evt.target.getPosition());
             openBubble(
@@ -92,15 +49,9 @@ function openBubble(position, text){
             
             map.addObject(group);
             
-            // get geo bounding box for the group and set it to the map
             map.setViewBounds(group.getBounds());
         }
         
-        /**
-        * Creates a series of list items for each location found, and adds it to the panel.
-        * @param {Object[]} locations An array of locations as received from the
-        *                             H.service.GeocodingService
-        */
         function addPlacesToPanel(places){
             
             var nodeOL = document.createElement('ul'),
@@ -126,34 +77,37 @@ function openBubble(position, text){
             
             placesContainer.appendChild(nodeOL);
         }
-    
         
-        // 1. initialize platform
         var platform = new H.service.Platform({
             app_id: 'A0an0Cy4CF1jmmOCpGBb',
             app_code: 'wIc8MPsnN0Ujx0gceJFuSg',
             useHTTPS: true,
             useCIT: true
         });
-        
+
         var defaultLayers = platform.createDefaultLayers();
         
-        var map = new H.Map(document.getElementById('map'),
-        defaultLayers.normal.map, {
-            center: {lat: -37.7942, lng: -122.4070},
-            zoom: 15
-        });
-        
-        var placesContainer = document.getElementById('panel');
-        
-        // add map behavior
-        
-        var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-        
-        // Create the default UI components
-        
-        var ui = H.ui.UI.createDefault(map, defaultLayers);
-        
+        // const mapCurrentLocation = (latitude, longitude) => {
+            // if (!latitude || !longitude){
+            //     latitude = -37.7942;
+            //     longitude = -122.4070;
+            // }
+            
+            var map = new H.Map(document.getElementById('map'),
+            defaultLayers.normal.map, {
+                center: {lat: -37.7942, lng: -122.4070},
+                zoom: 15
+            });
+            
+            var placesContainer = document.getElementById('panel');
+            
+            var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+            
+            var ui = H.ui.UI.createDefault(map, defaultLayers);
+            
+        // }
+
         searchPlaces(platform);
+
         
         
