9a38e146acdfb3c530f4497e6f4ce612b053d8fd
Duda Duarte <dudammduarte@yahoo.com.br>
2019-05-20 15:12:48 -0300

    iniciado o processo de agendamento

---
+++ public/src/scripts/booking/widget.js
@@ -0,0 +1,29 @@
+var widget = new SimplybookWidget ({
+    "widget_type":"iframe",
+    "url":"https:\/\/partiujogar.simplybook.me",
+    "theme":"default",
+    "theme_settings": {
+        "timeline_show_end_time":"0",
+        "timeline_modern_display":"as_slots",
+        "sb_base_color":"#33bb60",
+        "booking_nav_bg_color":"#d1e9c6",
+        "body_bg_color":"#f7f7f7",
+        "dark_font_color":"#494949",
+        "light_font_color":"#ffffff",
+        "btn_color_1":"#5e7da7",
+        "sb_company_label_color":"#ffffff",
+        "hide_img_mode":"1",
+        "show_sidebar":"1",
+        "sb_busy":"#dad2ce",
+        "sb_available":"#d3e0f1"},
+        "timeline":"modern_week",
+        "datepicker":"top_calendar",
+        "is_rtl":false,
+        "app_config": {
+            "predefined": {
+                "client": {
+                    "name": "Mary",
+                    "email": "mary@gmail.com",
+                    "phone": "+1234567890"
+                }}}
+        });
