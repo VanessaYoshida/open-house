ec3ccb3f5af88c12a0201a55393e5a5f242958d2
Vanessa <vanessayoshida92@gmail.com>
2019-05-22 02:37:17 -0300

    alterado funçoes de localizaçao, esta funcionando corretamente

--- public/src/scripts/maps/findCourts.js
+++ public/src/scripts/maps/findCourts.js
@@ -1,30 +1,27 @@
 
 const url = "https://places.cit.api.here.com/places/v1/browse?app_id=A0an0Cy4CF1jmmOCpGBb&app_code=wIc8MPsnN0Ujx0gceJFuSg";
 
-const search = "futebol";
 const templatePlaces = document.querySelector(".places");
 String.prototype.stripHTML = function() {return this.replace(/<.*?>/g, ' - ');}
 
 const showPlaces = (latitude, longitude) => {
     fetch(`${url}&in=${latitude},${longitude};r=5000&q=futebol&pretty`)
     .then(response => response.json())
-    .then((response) => {
+    .then(response => {
         response.results.items.forEach(item => {
-                const li = document.createElement('li');
-                li.innerHTML = `
-                    <br>
-                    <strong>Local: ${item.title}</strong><br>
-                    Endereço: ${item.vicinity.stripHTML()}<br>
-                    Distância: ${item.distance / 1000} km
-                `;
-                templatePlaces.appendChild(li);
-                window.marker = new H.map.Marker({ lat: item.position[0], lng: item.position[1] });
-                map.addObject(marker);
-       })
-      .catch(() => {
-          $('#panel').prepend(`Não foi encontrado nenhum resultado.`)
-       });    
- });     
-      
-    // showPlaces(-23.5507, -46.6333)
-    // showPlaces(localStorage.getItem("latitude"), localStorage.getItem("longitude"));
+            const li = document.createElement('li');
+            li.innerHTML = `
+            <br>
+            <strong>Local: ${item.title}</strong><br>
+            Endereço: ${item.vicinity.stripHTML()}<br>
+            Distância: ${item.distance / 1000} km
+            `;
+            templatePlaces.appendChild(li);
+            window.marker = new H.map.Marker({ lat: item.position[0], lng: item.position[1] });
+            map.addObject(marker);
+        })
+    })    
+    .catch(() => {
+        $('#panel').html(`Não foi encontrado nenhum resultado.`)
+    });     
+}    
--- public/src/scripts/maps/myLocation.js
+++ public/src/scripts/maps/myLocation.js
@@ -6,7 +6,7 @@ const locationUser = $("#location");
 
 const getLocation = () => {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showSP);
+        navigator.geolocation.getCurrentPosition(showPosition);
     } else {
         locationUser.innerHTML = "A geolocalização não é suportada neste browser.";
     }
@@ -16,35 +16,3 @@ const showPosition = (position) => {
     localStorage.setItem("latitude", position.coords.latitude);
     localStorage.setItem("longitude", position.coords.longitude);
 }
-
-function showSP(error) {
-    showPlaces(-23.5507, -46.6333)
-}
-
-// function showPosition(position) {
-//     showPlaces(position.coords.latitude, position.coords.longitude)
-// }
-
-// function showSP(error) {
-//     showPlaces(-23.5507, -46.6333)
-// }
-
-// const idapi = '0FuHpdUPSk8DjovzG8a3';
-// const codapi = '6YNHGglOpSTAE513hPercA';
-
-// const platform = new H.service.Platform({
-//     'app_id': idapi,
-//     'app_code': codapi
-// });
-
-// const defaultLayers = platform.createDefaultLayers();
-
-// function showMap(latitude, longitude){
-//     const map = new H.Map(
-//         document.getElementById('map'),
-//         defaultLayers.normal.map,
-//         {
-//             zoom: 14,
-//             center: { lat: latitude, lng: longitude}
-//         });
-// }
