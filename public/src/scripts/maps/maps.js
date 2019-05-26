7c021eb29e270bc413d0fc82960be5d5c388ce5f
Vanessa <vanessayoshida92@gmail.com>
2019-05-26 19:33:19 -0300

    tirando funçao em desuso

--- public/src/scripts/maps/maps.js
+++ public/src/scripts/maps/maps.js
@@ -52,8 +52,6 @@ let loadMap = () => {
       zoom: 14
     });
 
-  let locationsContainer = document.getElementById('panel');
-
   let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
   let ui = H.ui.UI.createDefault(map, defaultLayers);
@@ -77,61 +75,22 @@ let loadMap = () => {
     }
   }
 
-  let addLocationsToPanel = (locations) => {
-    let nodeOL = document.createElement('ul'),
-      i;
-
-    nodeOL.style.fontSize = 'small';
-    nodeOL.style.marginLeft = '5%';
-    nodeOL.style.marginRight = '5%';
-
-
-    for (i = 0; i < locations.length; i += 1) {
-      let li = document.createElement('li'),
-        divLabel = document.createElement('div'),
-        address = locations[i].location.address,
-        content = '<strong style="font-size: large;">' + address.label + '</strong></br>';
-      position = {
-        lat: locations[i].location.displayPosition.latitude,
-        lng: locations[i].location.displayPosition.longitude
-      };
-
-      content += '<strong>Rua:</strong> ' + address.street + '<br/>';
-      content += '<strong>Número:</strong> ' + address.houseNumber + '<br/>';
-      content += '<strong>Bairro:</strong> ' + address.district + '<br/>';
-      content += '<strong>Cidade:</strong> ' + address.city + '<br/>';
-      content += '<strong>Estado:</strong> ' + address.county + '<br/>';
-      content += '<strong>CEP:</strong> ' + address.postalCode + '<br/>';
-      content += '<strong>País:</strong> ' + address.country + '<br/>';
-      content += '<br/><strong>position:</strong> ' +
-        Math.abs(position.lat.toFixed(4)) + ((position.lat > 0) ? 'N' : 'S') +
-        ' ' + Math.abs(position.lng.toFixed(4)) + ((position.lng > 0) ? 'E' : 'W');
-
-      divLabel.innerHTML = content;
-      li.appendChild(divLabel);
-
-      nodeOL.appendChild(li);
-    }
-
-    locationsContainer.appendChild(nodeOL);
-  }
-
   let addLocationsToMap = (locations) => {
     let group = new H.map.Group(),
-      position,
-      i;
+      position;
 
-    for (i = 0; i < locations.length; i += 1) {
+    for (let loc of locations) {
       position = {
-        lat: locations[i].location.displayPosition.latitude,
-        lng: locations[i].location.displayPosition.longitude
+        lat: loc.location.displayPosition.latitude,
+        lng: loc.location.displayPosition.longitude
       };
       marker = new H.map.Marker(position);
-      marker.label = locations[i].location.address.label;
+      marker.label = loc.location.address.label;
       group.addObject(marker);
     }
 
-    group.addEventListener('tap', function (evt) {
+    group.addEventListener('click', function (evt) {
+      debugger;
       map.setCenter(evt.target.getPosition());
       openBubble(
         evt.target.getPosition(), evt.target.label);
